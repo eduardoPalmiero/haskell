389ebb49639e81659683a671a3c2d3f00cfefacf
Eduardo Palmiero <edupalmiero@gmail.com>
2019-09-09 19:58:34 -0300

    Clase

--- 09-09/clase.hs
+++ 09-09/clase.hs
@@ -1,2 +1,22 @@
-esPositivo x
-    | 
+
+-- sin mayor y sin menor. Sin ==.
+esPositivo x = esPositivo' (x,x)
+esPositivo' (_,0) = False
+esPositivo' (0,_) = True
+esPositivo' (x,y) = esPositivo' (pred x, succ y)
+
+max x y
+    | x >= y    = x
+    | otherwise = y
+--Tener en cuenta que tiene prioridad el primer elemento.
+
+-- import Data.Char
+-- ord :: Char -> Int
+--Devuelve el numero ASCII de un caracter
+-- chr hace lo contrario
+
+-- El backtick tmb hace que tengan mayor prioridad que las funciones normales. (??????)
+-- ademas hace algo raro con el punto.
+
+
+--para el TP solo podemos usar $, (.), succ, pred, :, ord, chr , (,) , ==
