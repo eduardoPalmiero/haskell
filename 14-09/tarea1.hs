8d6d1e606886b3292fb1cc7b9f3f98fe5549a2e8
Eduardo Palmiero <edupalmiero@gmail.com>
2019-09-14 20:23:18 -0300

    Agrego las funciones esPar y esMultiploDe

--- 14-09/tarea1.hs
+++ 14-09/tarea1.hs
@@ -4,6 +4,8 @@ esMultiploDe 35 7 -> "Si"
 esMultiploDe (-35) 5 -> "Si"
 esMultiploDe 14 3 -> "No"
 esMultiploDe x 2 = esPar x
+
+
 residuo 35 7 -> 0
 residuo (-35) 5 -> 0
 residuo 14 3 -> 2
@@ -16,7 +18,22 @@ Para quien se sienta motivado, otra función un poco más complicada: divEntero 
 
 divEntero 35 7 -> 5
 divEntero (-35) 5 -> (-7)
-divEntero 14 3 -> 4 -
-}
+divEntero 14 3 -> 4 
+-}
+import Debug.Trace
+--trace ("voy por " ++ show x) $
+
+esPar 0 = True
+esPar x
+    | x < 0     = esPar (-x)
+    | x >= 2    = esPar (x-2)
+    | otherwise = False
 
 
+esMultiploDe _ 0    = error ("No se puede dividir por cero.")
+esMultiploDe x y    
+    | x < 0         = esMultiploDe (-x) y
+    | y < 0         = esMultiploDe x (-y)
+    | x - y == 0    = True
+    | x > y         = esMultiploDe (x-y) y
+    | otherwise     = False
