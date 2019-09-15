374dfd92726af9edd4608b4c50d58b20fb70c425
Eduardo Palmiero <edupalmiero@gmail.com>
2019-09-14 21:59:48 -0300

    Agrego la funcion de residuo y de division por enteros

--- 14-09/tarea1.hs
+++ 14-09/tarea1.hs
@@ -1,4 +1,4 @@
-{- 
+{-
 Escribir las funciones esMultiploDe y residuo que satisfacen:
 esMultiploDe 35 7 -> "Si"
 esMultiploDe (-35) 5 -> "Si"
@@ -18,22 +18,40 @@ Para quien se sienta motivado, otra función un poco más complicada: divEntero 
 
 divEntero 35 7 -> 5
 divEntero (-35) 5 -> (-7)
-divEntero 14 3 -> 4 
+divEntero 14 3 -> 4
 -}
 import Debug.Trace
 --trace ("voy por " ++ show x) $
-
+esPar :: Int -> Bool
 esPar 0 = True
 esPar x
     | x < 0     = esPar (-x)
     | x >= 2    = esPar (x-2)
     | otherwise = False
 
-
+esMultiploDe :: Int -> Int -> Bool
 esMultiploDe _ 0    = error ("No se puede dividir por cero.")
-esMultiploDe x y    
-    | x < 0         = esMultiploDe (-x) y
-    | y < 0         = esMultiploDe x (-y)
-    | x - y == 0    = True
-    | x > y         = esMultiploDe (x-y) y
+esMultiploDe dividendo divisor
+    | dividendo < 0             = esMultiploDe (-dividendo) divisor
+    | divisor   < 0             = esMultiploDe dividendo (-divisor)
+    | dividendo - divisor == 0  = True
+    | dividendo > divisor       = esMultiploDe (dividendo - divisor) divisor
     | otherwise     = False
+
+residuo :: Int -> Int -> Int
+residuo _ 0     = error ("No se puede dividir por cero.")
+residuo x y
+    | y < 0     = residuo x (-y)
+    | x < 0     = -residuo (-x) y
+    | x >= y    = residuo (x-y) y
+    | x < y     = x
+    | otherwise = 0
+
+divEntero :: Int -> Int -> Int
+divEntero dividendo divisor
+    | divisor   == 0        = error ("No se puede dividir por cero.")
+    | dividendo <  0        = - divEntero(-dividendo) divisor
+    | divisor   <  0        = divEntero dividendo (-divisor)
+    | dividendo == divisor  = 1
+    | dividendo <  divisor  = 0
+    | dividendo > divisor   = 1 + divEntero (dividendo - divisor) divisor
