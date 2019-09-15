d18c9a3d35eaa2e70a651833540451a9c42560ba
Eduardo Palmiero <edupalmiero@gmail.com>
2019-09-15 00:01:47 -0300

    Agrego la funcion de multiplicar

--- 14-09/tarea2.hs
+++ 14-09/tarea2.hs
@@ -1,5 +1,12 @@
-{- 
-Ejercicios para la clase que viene: 
-escribir las funciones mult (multiplicación entre enteros) y esPrimo (definida solo para enteros positivos). 
+{-
+Ejercicios para la clase que viene:
+escribir las funciones mult (multiplicación entre enteros) y esPrimo (definida solo para enteros positivos).
 Mismas condiciones: sin multiplicar ni dividir, solo lo visto en clase
  -}
+
+mult :: Int -> Int -> Int
+mult _ 0 = 0
+mult x y
+    | y < 0 = -mult x (-y)
+    | x < 0 = -mult (-x) y
+    | y > 0 = x + mult x (y-1)
